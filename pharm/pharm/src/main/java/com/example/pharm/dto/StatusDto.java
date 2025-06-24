62d714482650be786ecb5b6b39fa9b23f7548252
Erick Alair Marçal <130059555+MeErickss@users.noreply.github.com>
2025-06-24 14:26:14 -0300

    reset

--- pharm/pharm/src/main/java/com/example/pharm/dto/StatusDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.pharm.dto;
-
-public class StatusDto {
-
-}
--- pharm/pharm/src/main/java/com/example/pharm/model/enumeration/NivelEnum.java
+++
@@ -1,7 +0,0 @@
-package com.example.pharm.model.enumeration;
-
-public enum NivelEnum {
-    ADMIN,
-    MANUTENCAO,
-    OPERADOR;
-}
