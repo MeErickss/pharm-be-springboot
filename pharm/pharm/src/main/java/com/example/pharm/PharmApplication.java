47c2b00ce6da689d37812b367bbdb73fdc6171f8
Erick Alair Marçal <130059555+MeErickss@users.noreply.github.com>
2025-08-14 19:34:12 -0300

    commit

--- pharm/pharm/src/main/java/com/example/pharm/PharmApplication.java
+++ pharm/pharm/src/main/java/com/example/pharm/PharmApplication.java
@@ -3,7 +3,7 @@ package com.example.pharm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;            // << adicionado
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
